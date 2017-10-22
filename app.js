c4e0f13df0ee9a76a049940f7c921122d34c47a9
Siva Boyapati <siboyapati@paypalcorp.con>
2017-10-21 17:34:13 -0700

    remove the name

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.use(express.static(path.join(__dirname, 'web')));
 app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
-//siva
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {

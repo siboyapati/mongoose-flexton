d6cf7e635faa1474a30a14a8eade58f518f6a74b
Siva Boyapati <siboyapati@paypalcorp.con>
2017-10-21 17:36:16 -0700

    remove the commit.

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/api', api);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
